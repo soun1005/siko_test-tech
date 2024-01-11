7c049efcd1dc28b583228547ffc638d386fabe83
Soeun Lee <thdms1005@gmail.com>
2024-01-11 10:56:45 +0100

    :sparkles: Add(partner) : add partner page
    need to style after

---
+++ src/components/pages/Partenaire.tsx
@@ -0,0 +1,22 @@
+const Partenaire = () => {
+  return (
+    <div className="bg-blueAccent mt-[87px] py-[120px]">
+      <div className="container displayFlex-[center] flex-col">
+        <h3 className="text-titleXL text-background">
+          Vous avez un site e-commerce ou une boutique ?
+        </h3>
+
+        <p className="text-white">
+          Rejoignez nos partenaires et offrez de multiples services et avantages
+          à vos clients.
+        </p>
+        <p className="text-white">
+          Augmentez votre chiffre d’affaire avec notre solution en financement.
+        </p>
+        <button className="text-white">Devenir Partenaire</button>
+      </div>
+    </div>
+  );
+};
+
+export default Partenaire;
